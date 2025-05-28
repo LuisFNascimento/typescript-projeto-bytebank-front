b5f532eca8494e0f95bfb2505d06cccaa6241738
Luís Fernando Nascimento <luisfernandonascimentopro@gmail.com>
2025-05-28 11:45:33 -0300

    Centralizando a lógica de saldo e operação

--- src/components/nova-transacao-component.ts
+++ src/components/nova-transacao-component.ts
@@ -1,6 +1,7 @@
+import Conta from "../types/Conta.js";
 import { TipoTransacao } from "../types/TipoTransacao.js";
 import { Transacao } from "../types/Transacao.js";
-import { atualizarSaldo, getSaldo } from "./saldo-component.js";
+import SaldoComponent from "./saldo-component.js";
 
 const elementoFormulario = document.querySelector(".block-nova-transacao form") as HTMLFormElement;
 elementoFormulario.addEventListener("submit", function (event) {
@@ -17,28 +18,14 @@ elementoFormulario.addEventListener("submit", function (event) {
   let tipoTransacao: TipoTransacao = inputTipoTransacao.value as TipoTransacao;
   let valor: number = inputValor.valueAsNumber;
   let data: Date = new Date(inputData.value);
-  let saldo: number = getSaldo();
-
-  if (tipoTransacao === TipoTransacao.DEPOSITO) {
-    saldo += valor;
-  } else if (
-    tipoTransacao === TipoTransacao.TRANSFERENCIA ||
-    tipoTransacao === TipoTransacao.PAGAMENTO_BOLETO
-  ) {
-    saldo -= valor;
-  } else {
-    alert("Tipo de Transação é inválida!");
-    return;
-  }
-
-  atualizarSaldo(saldo);
   
   const novaTransacao: Transacao = {
     tipoTransacao: tipoTransacao,
     valor: valor,
     data: data,
-  };
+  }
 
-  console.log(novaTransacao);
+  Conta.registrarTransacao(novaTransacao);
+  SaldoComponent.atualizar();
   elementoFormulario.reset();
 });
